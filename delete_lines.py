ff095b79a30e21c1ad665ff71dcf115e041d1a63
Antero Duarte <mail@afduarte.ovh>
2015-10-05 17:03:48 +0100

    Updated it to run as executable (without python script.py) and it can now take multiple strings as parameters

--- delete_lines.py
+++ delete_lines.py
@@ -1,17 +1,24 @@
-from sys import argv, stdout
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Delete Lines Python Script
 
-file = argv[1]
-find = argv[2]
+from sys import argv, stdout # Imports arguments and stdout from sys
 
-f = open(file,"r")
-lines = f.readlines()
-f.close()
+oldFile = argv[1] # The path for the file we want to modify
+find = argv[2:] # The array of different strings we want to look for (every argument after the first one)
 
-n = open(file+".new","w")
+f = open(oldFile,"r") # Open the old file in read mode
+lines = f.readlines() # Create array with every line in the old file
+f.close() # Close the old file
 
-for line in open(file):
-    if find not in line:
-        n.write(line)
+newFile = oldFile+".new" # New file name is oldfilename.extension.new
 
-n.close()
-print("Deleted every line that contained "+find)
+n = open(newFile,"w") # Open new file in write mode
+
+for string in find:            # For every string passed in and
+    for line in open(oldFile): # For every line in the old file
+        if string not in line: # Check for string
+            n.write(line)      # If it exists, don't write it to new file.
+
+n.close() # Close the new file
+print("Deleted every line that contained "+find+" output to file: "+newFile)
